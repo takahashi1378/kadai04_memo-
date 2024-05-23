1f028770d283414e07b547daab4865e057260eaa
takahashi1378 <168318880+takahashi1378@users.noreply.github.com>
2024-05-24 07:45:39 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+$(document).ready(function () {
+    $.getJSON("data.json", function(data){
+        for(var i in data){
+        $("#output").append("<li><strong>" + data[i].division + "</strong></li>");
+            for(var j in data[i].person){
+                $("#output").append("<li>" + data[i].person[j].name + "（" + data[i].person[j].age + "才）</li>n");
+            }
+        }
+    });
+});
+</script>
